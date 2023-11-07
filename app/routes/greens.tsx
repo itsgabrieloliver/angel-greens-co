7bca370a13e0df05609b422d3d3c589ead0ada16
Gabriel Oliver <straxxmusicteam@gmail.com>
2023-11-06 22:28:53 -0500

    updated responsive design

--- app/routes/greens.tsx
+++ app/routes/greens.tsx
@@ -9,6 +9,8 @@ import dunPeasPhoto from '../../public/product-photos/dun-peas-photo.jpg'
 import mildSaladMixPhoto from '../../public/product-photos/mild-salad-mix-photo.jpeg'
 import radishPhoto from '../../public/product-photos/radish-photo.jpeg'
 import spicySaladMix from '../../public/product-photos/spicy-salad-mix-photo.jpg'
+import arugulaPhoto from '../../public/product-photos/arugula-photo.jpg'
+import sunflowerPhoto from '../../public/product-photos/sunflower-photo.jpg'
 
 export const meta: V2_MetaFunction = () => {
     return [
@@ -25,65 +27,85 @@ export default function Greens() {
                     <h3 className={'text-2xl font-bold text-[#4E8068] drop-shadow-2xl'}>Our Greens</h3>
                     <p>Explore our delicious variety.</p>
                 </div>
-                <div className={'w-full p-2 flex flex-col items-center justify-center gap-8'}>
-                    <div className={'rounded-xl w-full md:w-2/3 h-[12rem] bg-[#2D240E] flex p-1'}>
-                        <div className={'w-full px-2 flex flex-col gap-4 items-center justify-center'}>
-                            <p className={'text-white text-xl uppercase font-bold'}>Cilantro <span className={'ml-2 rounded-full px-2 bg-[#4E8068] text-sm'}>New!</span></p>
-                            <p className={'text-sm text-white/80 text-center'}>A sweet, bright citrusy aroma with a clean, bold, classic cilantro flavor finishing with robust notes of pepper and fresh cut grass.<br /><span className={'underline'}>Great with:</span> Salad, Bagels, Sandwiches, Tacos, Pizza, Garnish, Etc.</p>
-                        </div>
-                        <div className={'rounded-lg h-full w-[30rem] bg-cover bg-center'} style={{ backgroundImage: `url(${cilantroPhoto})` }}></div>
-                    </div>
-                    <div className={'rounded-xl w-full md:w-2/3 h-[12rem] bg-[#2D240E] flex p-1'}>
-                        <div className={'rounded-lg h-full w-[30rem] bg-cover bg-center'} style={{ backgroundImage: `url(${spicySaladMix})` }}></div>
-                        <div className={'w-full px-2 flex flex-col gap-4 items-center justify-center'}>
-                            <p className={'text-white text-xl uppercase font-bold'}>Spicy Salad Mix <span className={'ml-2 rounded-full px-2 bg-[#4E8068] text-sm'}>Top Seller!</span></p>
-                            <p className={'text-sm text-white/80 text-center'}>A flavorful and colorful combination of microgreens with a spicy flavor.<br /><span className={'underline'}>Contains:</span> Broccoli, Kale, Kohlrabi, Arugula, Red Cabbage & Mustard for an extra kick.<br /><span className={'underline'}>Great with:</span> Salad, Bagels, Sandwiches, Tacos, Pizza, Garnish, Etc.</p>
+                <div className={'w-full h-auto grid grid-cols-1 md:grid-cols-4 grid-rows-2 p-4 md:p-20 gap-x-6 gap-y-4'}>
+                    <div className={'h-auto row-span-1 col-span-1 bg-[#2D240E] rounded-xl p-1'}>
+                        <div className={'h-[14rem] rounded-md w-full bg-center bg-cover'} style={{ backgroundImage: `url(${cilantroPhoto})` }}></div>
+                        <div className={'p-4 text-white flex flex-col gap-3'}>
+                            <div className={'flex gap-2'}>
+                                <h2 className={'text-lg font-medium'}>Cilantro</h2>
+                                <p className={'bg-[#4E8068] flex items-center justify-center rounded-full px-2 py-0 text-sm'}>NEW!</p>
+                            </div>
+                            <p className={'text-sm text-white/80 font-thin'}>A sweet, bright citrusy aroma with a clean, bold, classic cilantro flavor finishing with robust notes of pepper and fresh cut grass.</p>
                         </div>
                     </div>
-                    <div className={'rounded-xl w-full md:w-2/3 h-[12rem] bg-[#2D240E] flex p-1'}>
-                        <div className={'w-full px-2 flex flex-col gap-4 items-center justify-center'}>
-                            <p className={'text-white text-xl uppercase font-bold'}>Mild Salad Mix</p>
-                            <p className={'text-sm text-white/80 text-center'}>A flavorful and colorful combination of microgreens with a hint of spice.<br /><span className={'underline'}>Contains:</span> Broccoli, Kale, Kohlrabi, Arugula & Cabbage. Top Seller!<br /><span className={'underline'}>Great with:</span> Salad, Bagels, Sandwiches, Tacos, Pizza, Garnish, Etc.</p>
-                        </div>
-                        <div className={'rounded-lg h-full w-[30rem] bg-cover bg-center'} style={{ backgroundImage: `url(${mildSaladMixPhoto})` }}></div>
-                    </div>
-                    <div className={'rounded-xl w-full md:w-2/3 h-[12rem] bg-[#2D240E] flex p-1'}>
-                        <div className={'rounded-lg h-full w-[30rem] bg-cover bg-center'} style={{ backgroundImage: `url(${broccoliPhoto})` }}></div>
-                        <div className={'w-full px-2 flex flex-col gap-4 items-center justify-center'}>
-                            <p className={'text-white text-xl uppercase font-bold'}>Broccoli <span className={'ml-2 rounded-full px-2 bg-[#4E8068] text-sm'}>One of our healthiest greens!</span></p>
-                            <p className={'text-sm text-white/80 text-center'}>A slight spicy taste with a minor hint of broccoli flavor.<br /><span className={'underline'}>Great with:</span> Salads, Bagels, Sandwiches, Smoothies, Tacos, Pizza, Soups, Garnishes, Etc.</p>
+                    <div className={'h-auto row-span-1 col-span-1 bg-[#2D240E] rounded-xl p-1'}>
+                        <div className={'h-[14rem] rounded-md w-full bg-center bg-cover'} style={{ backgroundImage: `url(${sunflowerPhoto})` }}></div>
+                        <div className={'p-4 text-white flex flex-col gap-3'}>
+                            <div className={'flex gap-2'}>
+                                <h2 className={'text-lg font-medium'}>Sunflower</h2>
+                                <p className={'bg-[#4E8068] flex items-center justify-center rounded-full px-2 py-0 text-sm'}>NEW!</p>
+                            </div>
+                            <p className={'text-sm text-white/80 font-thin'}>A nice mix between nutty and lemony with a sweet taste and crunch at the end.</p>
                         </div>
                     </div>
-                    <div className={'rounded-xl w-full md:w-2/3 h-[12rem] bg-[#2D240E] flex p-1'}>
-                        <div className={'w-full px-2 flex flex-col gap-4 items-center justify-center'}>
-                            <p className={'text-white text-xl uppercase font-bold'}>Dun Pea Shoots</p>
-                            <p className={'text-sm text-white/80 text-center'}>Some temporary text, this text is only temporary.</p>
-                        </div>
-                        <div className={'rounded-lg h-full w-[30rem] bg-cover bg-center'} style={{ backgroundImage: `url(${dunPeasPhoto})` }}></div>
-                    </div>
-                    <div className={'rounded-xl w-full md:w-2/3 h-[12rem] bg-[#2D240E] flex p-1'}>
-                        {/* Todo: change this temporary image */}
-                        <div className={'rounded-lg h-full w-[30rem] bg-cover bg-center'} style={{ backgroundImage: `url(${tempProductPhoto})` }}></div>
-                        <div className={'w-full px-2 flex flex-col gap-4 items-center justify-center'}>
-                            <p className={'text-white text-xl uppercase font-bold'}>Arugula <span className={'ml-2 rounded-full px-2 bg-[#4E8068] text-sm'}>Our favorite!</span></p>
-                            <p className={'text-sm text-white/80 text-center'}>A sweet, tangy, peppery, and earthy flavor with grassy, vegetal, and nutty hints.<br /><span className={'underline'}>Great with:</span> Salads, Bagels, Sandwiches, Tacos, Pizza, Soups, Garnishes, Etc.
-                            </p>
+                    <div className={'h-auto row-span-1 col-span-1 bg-[#2D240E] rounded-xl p-1'}>
+                        <div className={'h-[14rem] rounded-md w-full bg-center bg-cover'} style={{ backgroundImage: `url(${mildSaladMixPhoto})` }}></div>
+                        <div className={'p-4 text-white flex flex-col gap-3'}>
+                            <div className={'flex gap-2'}>
+                                <h2 className={'text-lg font-medium'}>Mild Salad Mix</h2>
+                                <p className={'bg-[#4E8068] flex items-center justify-center rounded-full px-2 py-0 text-sm'}>Top Seller!</p>
+                            </div>
+                            <p className={'text-sm text-white/80 font-thin'}>A flavorful and colorful combination of microgreens with a hint of spice.</p>
                         </div>
                     </div>
-                    <div className={'rounded-xl w-full md:w-2/3 h-[12rem] bg-[#2D240E] flex p-1'}>
-                        <div className={'w-full px-2 flex flex-col gap-4 items-center justify-center'}>
-                            <p className={'text-white text-xl uppercase font-bold'}>Purple Rambo Radish</p>
-                            <p className={'text-sm text-white/80 text-center'}>A crisp, peppery, crunchy radish taste, similar to a radish bulb.<br /><span className={'underline'}>Great with:</span> Salads, Bagels, Sandwiches, Tacos, Pizza, Soups, Garnish Etc.</p>
+                    <div className={'h-auto row-span-1 col-span-1 bg-[#2D240E] rounded-xl p-1'}>
+                        <div className={'h-[14rem] rounded-md w-full bg-center bg-cover'} style={{ backgroundImage: `url(${spicySaladMix})` }}></div>
+                        <div className={'p-4 text-white flex flex-col gap-3'}>
+                            <div className={'flex gap-2'}>
+                                <h2 className={'text-lg font-medium'}>Spicy Salad Mix</h2>
+                                <p className={'bg-[#4E8068] flex items-center justify-center rounded-full px-2 py-0 text-sm'}>Top Seller!</p>
+                            </div>
+                            <p className={'text-sm text-white/80 font-thin'}>A flavorful and colorful combination of microgreens with a spicy flavor.</p>
                         </div>
-                        <div className={'rounded-lg h-full w-[30rem] bg-cover bg-center'} style={{ backgroundImage: `url(${radishPhoto})` }}></div>
                     </div>
-                    {/*<div className={'rounded-xl w-full md:w-2/3 h-[12rem] bg-[#2D240E] flex p-1'}>*/}
-                    {/*    <div className={'rounded-lg h-full w-[30rem] bg-cover bg-center'} style={{ backgroundImage: `url(${tempProductPhoto})` }}></div>*/}
-                    {/*    <div className={'w-full px-2 flex flex-col gap-4 items-center justify-center'}>*/}
-                    {/*        <p className={'text-white text-xl uppercase font-bold'}>Wheat Grass</p>*/}
-                    {/*        <p className={'text-sm text-white/80 text-center'}>Some temporary text, this text is only temporary.</p>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    <div className={'h-auto row-span-1 col-span-1 bg-[#2D240E] rounded-xl p-1'}>
+                        <div className={'h-[14rem] rounded-md w-full bg-center bg-cover'} style={{ backgroundImage: `url(${arugulaPhoto})` }}></div>
+                        <div className={'p-4 text-white flex flex-col gap-3'}>
+                            <div className={'flex gap-2'}>
+                                <h2 className={'text-lg font-medium'}>Arugula</h2>
+                                <p className={'bg-[#4E8068] flex items-center justify-center rounded-full px-2 py-0 text-sm'}>Our Favorite!</p>
+                            </div>
+                            <p className={'text-sm text-white/80 font-thin'}>A sweet, tangy, peppery, and earthy flavor with grassy, vegetal, and nutty.</p>
+                        </div>
+                    </div>
+                    <div className={'h-auto row-span-1 col-span-1 bg-[#2D240E] rounded-xl p-1'}>
+                        <div className={'h-[14rem] rounded-md w-full bg-center bg-cover'} style={{ backgroundImage: `url(${broccoliPhoto})` }}></div>
+                        <div className={'p-4 text-white flex flex-col gap-3'}>
+                            <div className={'flex gap-2'}>
+                                <h2 className={'text-lg font-medium'}>Broccoli</h2>
+                                <p className={'bg-[#4E8068] flex items-center justify-center rounded-full px-2 py-0 text-sm'}>Healthy Choice!</p>
+                            </div>
+                            <p className={'text-sm text-white/80 font-thin'}>A slight spicy taste with a minor hint of broccoli flavor.</p>
+                        </div>
+                    </div>
+                    <div className={'h-auto row-span-1 col-span-1 bg-[#2D240E] rounded-xl p-1'}>
+                        <div className={'h-[14rem] rounded-md w-full bg-center bg-cover'} style={{ backgroundImage: `url(${radishPhoto})` }}></div>
+                        <div className={'p-4 text-white flex flex-col gap-3'}>
+                            <div className={'flex gap-2'}>
+                                <h2 className={'text-lg font-medium'}>Radish</h2>
+                            </div>
+                            <p className={'text-sm text-white/80 font-thin'}>A crisp, peppery, crunchy radish taste, similar to a radish bulb.</p>
+                        </div>
+                    </div>
+                    <div className={'h-auto row-span-1 col-span-1 bg-[#2D240E] rounded-xl p-1'}>
+                        <div className={'h-[14rem] rounded-md w-full bg-center bg-cover'} style={{ backgroundImage: `url(${dunPeasPhoto})` }}></div>
+                        <div className={'p-4 text-white flex flex-col gap-3'}>
+                            <div className={'flex gap-2'}>
+                                <h2 className={'text-lg font-medium'}>Dun Pea Shoots</h2>
+                            </div>
+                            <p className={'text-sm text-white/80 font-thin'}>A mild and pleasant sweet pea flavor with a crispy and crunchy finish.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Layout>
