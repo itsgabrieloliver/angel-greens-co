c8532489bdd754939582ead9b54152dddcaa942c
Gabriel Oliver <straxxmusicteam@gmail.com>
2024-03-20 19:55:06 -0400

    add canonical tag (help fix ip address in seo)

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <link rel="canonical" href="https://angelgreens.co/" />
         <Meta />
         <Links />
       </head>
